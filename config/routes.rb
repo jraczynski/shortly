ea4b603d388546431abf889929391efcacce32d9
Jakub Raczyński <jkb.raczynski@gmail.com>
2014-02-03 22:43:10 +0100

    Finish layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 Shortly::Application.routes.draw do
-  get "static_pages/home"
+  root  'static_pages#home'
+  #match '/help',    to: 'static_pages#help',    via: 'get'
 
 
-
-
-
-
-
-
-
-  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
