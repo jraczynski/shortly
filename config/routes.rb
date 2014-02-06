879c6053ae6cb63689e0d805f0a090cc5e6a3e56
Jakub Raczyński <jkb.raczynski@gmail.com>
2014-02-06 20:19:51 +0100

    Add User model and signup route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Shortly::Application.routes.draw do
+  get "users/new"
   get 'l/:short_url', to: 'links#redirect', as: 'redirlink'
   get 's/:short_url', to: 'links#show', as: 'showlink'
   resources :links, :except => [:show] #no show by id to increase user link privacy
   root  'static_pages#home'
-  #match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/signup',  to: 'users#new', via: 'get'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
